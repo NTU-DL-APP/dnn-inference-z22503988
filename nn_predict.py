733764c7201fa243a6681f66102b24641e951c39
109300308 <102142044+z22503988@users.noreply.github.com>
2025-06-10 14:37:18 +0800

    Add files via upload

--- nn_predict.py
+++ nn_predict.py
@@ -3,12 +3,12 @@ import json
 
 # === Activation functions ===
 def relu(x):
-    # TODO: Implement the Rectified Linear Unit
-    return x
+    def relu(x):
+    return np.maximum(0, x)
 
 def softmax(x):
-    # TODO: Implement the SoftMax function
-    return x
+    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True)) 
+    return e_x / np.sum(e_x, axis=-1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
